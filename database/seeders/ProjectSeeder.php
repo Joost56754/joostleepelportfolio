552475fa78ffff5fcfaf5aa0a72d0956c7cd3149
Joost <93526867+Joost56754@users.noreply.github.com>
2023-09-20 21:42:46 +0200

    Maakt een project model en een seeder aan

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Project::create([
+            'title' => 'Event 1',
+            'description' => 'Dit is een test',
+            'image' => 'img/Int.jpg',
+        ]);
+        Project::create([
+            'title' => 'Event 1',
+            'description' => 'Dit is een test',
+            'image' => 'img/zal.jpg',
+        ]);
+        Project::create([
+            'title' => 'Event 1',
+            'description' => 'Dit is een test',
+            'image' => 'img/bolcom.jpg',
+        ]);
+    }
+}
